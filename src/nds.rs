b453c575d3cd17d284a896f1dd86b5b64c8d969d
Anne Castrillon <anne.castrillon@gmail.com>
2024-06-30 22:11:23 -0700

    minor changes

--- src/nds.rs
+++ src/nds.rs
@@ -11,11 +11,16 @@ pub struct Nds {
 impl Nds {
   pub fn new(firmware_bytes: Vec<u8>, bios7_bytes: Vec<u8>, bios9_bytes: Vec<u8>, rom_bytes: Vec<u8>) -> Self {
     let bus = Rc::new(RefCell::new(Bus::new(firmware_bytes, bios7_bytes, bios9_bytes, rom_bytes)));
-    Self {
+    let mut nds = Self {
       arm9_cpu: CPU::new(bus.clone()),
       arm7_cpu: CPU::new(bus.clone()),
       bus
-    }
+    };
+
+    nds.arm7_cpu.reload_pipeline32();
+    nds.arm9_cpu.reload_pipeline32();
+
+    nds
   }
 
   pub fn step(&mut self) {
