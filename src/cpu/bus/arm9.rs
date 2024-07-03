48b7168cce2946dc80c5467254a46bc0d0a35e91
Anne Castrillon <anne.castrillon@gmail.com>
2024-07-02 22:08:26 -0700

    stub keyinput for now

--- src/cpu/bus/arm9.rs
+++ src/cpu/bus/arm9.rs
@@ -66,6 +66,7 @@ impl Bus {
     match address {
       0x400_0004 => self.gpu.dispstat[1].read(),
       0x400_0300 => self.arm9.postflg as u16,
+      0x400_0130 => 0xffff, // controller, hardcoded for now. TODO!
       _ => {
         panic!("io register not implemented: {:X}", address);
       }
