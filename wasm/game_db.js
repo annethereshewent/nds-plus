2129d718ef357e2f25e8468ec84da087236e8e4e
Anne Castrillon <anne.castrillon@gmail.com>
2024-08-24 14:04:18 -0700

    remove console.logs

--- wasm/game_db.js
+++ wasm/game_db.js
@@ -2,10 +2,7 @@ const request = indexedDB.open("ds_saves", 2)
 
 
 request.onupgradeneeded = (event) => {
-  console.log('test????')
   const db = event.target.result
-
-  console.log(db)
 
   db.createObjectStore("saves", { keyPath: "gameName" })
 }
@@ -37,7 +34,6 @@ function getSave(gameName) {
 }
 
 function setSave(gameName, data) {
-  console.log('im being called a bunch somehow')
   return new Promise((resolve, reject) => {
     const request = indexedDB.open("ds_saves")
 
@@ -51,14 +47,12 @@ function setSave(gameName, data) {
       const request = objectStore.get(gameName)
 
       request.onsuccess = (event) => {
-        console.log(request)
         const existing = request.result
 
         if (existing != null) {
           existing.data = data
           objectStore.put(existing)
         } else {
-          console.log('storing save in database')
           objectStore.add({
             gameName,
             data
@@ -68,15 +62,12 @@ function setSave(gameName, data) {
       }
 
       request.onerror = (event) => {
-        console.log('record not found, creating')
-
         const request = objectStore.add({
           gameName,
           data
         })
 
         request.onsuccess = (event) => {
-          console.log('successfully added save to db')
           resolve(true)
         }
       }
